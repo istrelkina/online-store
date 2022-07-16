daf511af2b281623e041a7890a487f14f87c6a1c
Irina Strelkina <strelaweb@gmail.com>
2022-07-16 21:08:38 +1000

    feat: add eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import "./index.html";
-import "./style.scss";
-import { mult, sum } from "./modules/calc";
-
-console.log(mult(4, 5));
-console.log(sum(4, 5));
+import './index.html';
+import './style.scss';
